eac3b1778e7b4866a081bd2366eaa2fdd0bda13d
FeliSenaa <felipesena1025@gmail.com>
2024-05-16 21:54:59 -0300

    Initial commit

---
+++ src/main/java/com/mides/core/ServicioInsercionLaboralPersonasConDiscapacidadApplication.java
@@ -0,0 +1,13 @@
+package com.mides.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioInsercionLaboralPersonasConDiscapacidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioInsercionLaboralPersonasConDiscapacidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mides/core/ServicioInsercionLaboralPersonasConDiscapacidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mides.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioInsercionLaboralPersonasConDiscapacidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
