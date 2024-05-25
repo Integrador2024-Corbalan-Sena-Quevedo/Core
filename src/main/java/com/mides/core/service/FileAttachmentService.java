0613c68953ddf51c167310fb293d74ded3292248
FeliSena <felipesena1025@gmail.com>
2024-05-25 20:24:13 -0300

    Cambio de responsabilidad entre clases
    Cambio de responsabilidad entre clases

--- src/main/java/com/mides/core/service/FileAttachmentService.java
+++ src/main/java/com/mides/core/service/FileAttachmentService.java
@@ -2,16 +2,38 @@ package com.mides.core.service;
 
 
 import com.mides.core.model.Candidato;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Service
 public class FileAttachmentService implements IFileAttachmentService{
+
     @Autowired
     ICandidatoSevice candidatoSevice;
+
+    List<Map<String,String>> csvData = new ArrayList<>();
+
+    @Override
+    public void forCSVData(BufferedReader bufferedReader, CSVParser csvParser) {
+
+        for (CSVRecord csvRecord : csvParser) { // csvRecord son los values del archivo
+            Map<String,String> csvRow = new HashMap<>();
+            for (String header : csvParser.getHeaderNames()){
+                csvRow.put(header, csvRecord.get(header));
+            }
+            csvData.add(csvRow);
+        }
+
+        this.processCSVData(csvData);
+    }
 
     @Override
     public void processCSVData(List<Map<String, String>> csvData) {
