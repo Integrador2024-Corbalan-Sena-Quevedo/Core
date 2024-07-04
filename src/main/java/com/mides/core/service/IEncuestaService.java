719f107bd39fffc4f2dbe05c6fe689cb1d03d2ab
FeliSena <felipesena1025@gmail.com>
2024-07-04 19:42:54 -0300

    empresa file mapeo

--- src/main/java/com/mides/core/service/IEncuestaService.java
+++ src/main/java/com/mides/core/service/IEncuestaService.java
@@ -1,14 +1,18 @@
 package com.mides.core.service;
 
 import com.mides.core.model.Candidato;
-import com.mides.core.model.Encuesta;
+import com.mides.core.model.Empresa;
+import com.mides.core.model.EncuestaCandidato;
+import com.mides.core.model.EncuestaEmpresa;
 
 import java.text.ParseException;
 import java.util.List;
 import java.util.Map;
 
 public interface IEncuestaService {
-    void saveEncuesta(Encuesta encuesta);
-    void processEncuesta(List<Map<String,String>> csvData, Candidato candidato) throws ParseException;
+    void saveEncuesta(EncuestaCandidato encuestaCandidato);
+    void saveEncuestaEmpresa(EncuestaEmpresa encuestaEmpresa);
+    void processEncuestaCandidato(List<Map<String,String>> csvData, Candidato candidato) throws ParseException;
+    void processEncuestaEmpresa(List<Map<String,String>> csvData, Empresa empresa) throws ParseException;
 
 }
